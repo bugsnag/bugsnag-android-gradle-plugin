080a18e75c86b9e1c90122565cce840f2faf9861
James Smith <james@loopj.com>
2014-12-30 16:39:20 -0800

    Actually upload mapping file

--- src/main/groovy/com/bugsnag/android/gradle/BugsnagPlugin.groovy
+++ src/main/groovy/com/bugsnag/android/gradle/BugsnagPlugin.groovy
@@ -1,62 +1,82 @@
 package com.bugsnag.android.gradle
 
+import java.io.File
+
+import groovy.util.XmlParser
+import groovy.xml.Namespace
+
+import org.apache.http.HttpResponse
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody
+import org.apache.http.entity.mime.content.StringBody
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class BugsnagPlugin implements Plugin<Project> {
-  private static final String TASK_NAME = 'bugsnag'
-  private static final String API_KEY_TAG = 'com.bugsnag.android.API_KEY'
+    private static final String API_KEY_TAG = 'com.bugsnag.android.API_KEY'
 
-  void apply(Project project) {
-    project.android.applicationVariants.all { variant ->
-      // Only create Bugsnag tasks for proguard-enabled variants
-      if (!variant.buildType.minifyEnabled) {
-        return
-      }
+    void apply(Project project) {
+        project.extensions.create("bugsnag", BugsnagPluginExtension)
 
-      // Get the variant name
-      def variantName = getVariantName(variant)
+        project.android.applicationVariants.all { variant ->
+            // Only create Bugsnag tasks for proguard-enabled variants
+            if (variant.getObfuscation() == null) {
+                return
+            }
 
-      // Find the processed manifest for this variant
-      def manifestPath = variant.outputs[0].processManifest.manifestOutputFile
+            // Create Bugsnag post-build task
+            def variantName = variant.name.capitalize()
+            def bugsnagTask = project.task("uploadBugsnag${variantName}Mapping") << {
+                // Find the processed manifest for this variant
+                def manifestPath = variant.outputs[0].processManifest.manifestOutputFile
 
-      // Create Bugsnag post-build task
-      def bugsnagTask = project.task("$TASK_NAME$variantName") << {
-        // Fetch the Bugsnag API key from the AndroidManifest
-        def ns = new groovy.xml.Namespace("http://schemas.android.com/apk/res/android", "android")
-        def xml = new XmlParser().parse(manifestPath)
-        def apiKeyTags = xml.application['meta-data'].findAll{ it.attributes()[ns.name].equals(API_KEY_TAG) }
-        if (apiKeyTags.size() == 0) {
-          project.logger.warn("Could not find '$API_KEY_TAG' <meta-data> tag in your AndroidManifest.xml")
-          return
+                // Parse the AndroidManifest.xml
+                def ns = new Namespace("http://schemas.android.com/apk/res/android", "android")
+                def xml = new XmlParser().parse(manifestPath)
+
+                // Get the Bugsnag API key
+                def apiKey
+                if(project.bugsnag.apiKey != null) {
+                    apiKey = project.bugsnag.apiKey
+                } else {
+                    def metaDataTags = xml.application['meta-data']
+                    def apiKeyTags = metaDataTags.findAll{ it.attributes()[ns.name].equals(API_KEY_TAG) }
+                    if (apiKeyTags.size() == 0) {
+                        project.logger.warn("Could not find '$API_KEY_TAG' <meta-data> tag in your AndroidManifest.xml")
+                        return
+                    }
+                    apiKey = apiKeyTags[0].attributes()[ns.value]
+                }
+
+                // Get the build version
+                def versionName = xml.attributes()[ns.versionName]
+                if (versionName == null) {
+                    project.logger.warn("Could not find 'android:versionName' value in your AndroidManifest.xml")
+                    return
+                }
+
+                // Find the Proguard mapping file
+                File mappingFile = variant.getMappingFile()
+
+                // Upload the mapping file to Bugsnag
+                MultipartEntity mpEntity = new MultipartEntity();
+                mpEntity.addPart("proguard", new FileBody(mappingFile));
+                mpEntity.addPart("apiKey", new StringBody(apiKey));
+                mpEntity.addPart("version", new StringBody(versionName));
+
+                HttpPost httpPost = new HttpPost(project.bugsnag.endpoint)
+                httpPost.setEntity(mpEntity);
+
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpResponse response = httpClient.execute(httpPost);
+            }
+
+            // Run Bugsnag post-build tasks as part of a build
+            project.tasks.build.dependsOn bugsnagTask
         }
-
-        def apiKey = apiKeyTags[0].attributes()[ns.value]
-        println apiKey
-
-        // TODO: Make sure a proguard mapping file is output
-        // build/outputs/proguard/release/mapping.txt
-
-        // TODO: Get the build version
-
-        // TODO: Upload the mapping file to Bugsnag
-      }
-
-      // Run Bugsnag post-build tasks as part of a build
-      project.tasks.build.dependsOn bugsnagTask
     }
-  }
-
-  // Get the "variant" name (flavor + buildtype)
-  def getVariantName(variant) {
-    def buildTypeName = variant.buildType.name.capitalize()
-
-    def projectFlavorNames = variant.productFlavors.collect { it.name.capitalize() }
-    if (projectFlavorNames.isEmpty()) {
-      projectFlavorNames = [""]
-    }
-    def projectFlavorName = projectFlavorNames.join()
-
-    return "$projectFlavorName$buildTypeName"
-  }
 }
---
+++ src/main/groovy/com/bugsnag/android/gradle/BugsnagPluginExtension.groovy
@@ -0,0 +1,6 @@
+package com.bugsnag.android.gradle
+
+class BugsnagPluginExtension {
+    def String endpoint = 'https://upload.bugsnag.com'
+    def String apiKey = null
+}
