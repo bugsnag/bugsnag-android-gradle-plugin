ea330a60aa1e2891216c68c56e66c4238f8d975c
Delisa Mason <iskanamagus@gmail.com>
2016-08-31 07:58:01 +0200

    Fix syntax error when selecting whether to run

--- src/main/groovy/com/bugsnag/android/gradle/BugsnagManifestTask.groovy
+++ src/main/groovy/com/bugsnag/android/gradle/BugsnagManifestTask.groovy
@@ -68,7 +68,7 @@ class BugsnagManifestTask extends DefaultTask {
         def ns = new Namespace("http://schemas.android.com/apk/res/android", "android")
         def app = new XmlParser().parse(manifestPath).application[0]
         if (app) {
-            let tagCount = app['meta-data'].findAll {
+            def tagCount = app['meta-data'].findAll {
                 it.attributes()[ns.name].equals(BugsnagPlugin.BUILD_UUID_TAG)
             }.size()
             tagCount == 0 || !isInstantRun()
