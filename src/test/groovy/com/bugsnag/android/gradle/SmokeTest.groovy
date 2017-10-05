17094c38c1f85293ca36d75fe3f6663f739e2478
fractalwrench <fractalwrench@gmail.com>
2017-10-05 13:56:11 +0100

    setup example test

---
+++ src/test/groovy/com/bugsnag/android/gradle/SmokeTest.groovy
@@ -0,0 +1,14 @@
+package com.bugsnag.android.gradle
+
+import org.junit.Assert
+import org.junit.Test
+
+
+class SmokeTest {
+
+    @Test
+    public void greeterPluginAddsGreetingTaskToProject() {
+        Assert.assertTrue(2 + 2 == 4)
+    }
+
+}
