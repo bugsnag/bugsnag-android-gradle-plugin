457115d2b7d2ba31399c6e4bf007763b84728444
fractalwrench <fractalwrench@gmail.com>
2017-10-09 14:09:11 +0100

    update unit test to apply plugin and check that extension has been applied

--- src/test/groovy/com/bugsnag/android/gradle/SmokeTest.groovy
+++ src/test/groovy/com/bugsnag/android/gradle/SmokeTest.groovy
@@ -1,14 +1,18 @@
 package com.bugsnag.android.gradle
 
-import org.junit.Assert
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Test
 
+import static org.junit.Assert.assertEquals
 
 class SmokeTest {
 
     @Test
-    public void greeterPluginAddsGreetingTaskToProject() {
-        Assert.assertTrue(2 + 2 == 4)
+    void ensurePluginBuilds() {
+        Project project = ProjectBuilder.builder().build()
+        project.pluginManager.apply 'com.bugsnag.android.gradle'
+        assertEquals("https://upload.bugsnag.com", project.bugsnag.endpoint)
     }
 
 }
