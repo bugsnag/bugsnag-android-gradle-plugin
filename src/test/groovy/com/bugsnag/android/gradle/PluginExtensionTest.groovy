a20a0eb0fe647f2fa2b55d1edc4c7ffc63263095
fractalwrench <fractalwrench@gmail.com>
2017-11-16 12:20:08 +0000

    create plugin extension class

---
+++ src/test/groovy/com/bugsnag/android/gradle/PluginExtensionTest.groovy
@@ -0,0 +1,34 @@
+package com.bugsnag.android.gradle
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Before
+import org.junit.Test
+
+import static org.junit.Assert.*
+
+class PluginExtensionTest {
+
+    private Project proj
+
+    @Before
+    void setUp() throws Exception {
+        proj = ProjectBuilder.builder().build()
+        proj.pluginManager.apply 'com.bugsnag.android.gradle'
+    }
+
+    @Test
+    void ensureExtensionDefaults() {
+        assertEquals("https://upload.bugsnag.com", proj.bugsnag.endpoint)
+        assertNull(proj.bugsnag.apiKey)
+        assertTrue(proj.bugsnag.autoUpload)
+        assertTrue(proj.bugsnag.autoProguardConfig)
+        assertFalse(proj.bugsnag.uploadDebugBuildMappings)
+        assertFalse(proj.bugsnag.overwrite)
+        assertEquals(0, proj.bugsnag.retryCount)
+        assertFalse(proj.bugsnag.ndk)
+        assertNull(proj.bugsnag.sharedObjectPath)
+    }
+
+
+}
