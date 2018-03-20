a0b43f9b8a68e33d35ee396e07461f70a2e2d5f8
Jamie Lynch <fractalwrench@gmail.com>
2018-03-20 16:50:02 +0000

    add step for manual mapping upload task (#90)

--- features/steps/gradle_plugin_steps.rb
+++ features/steps/gradle_plugin_steps.rb
@@ -6,3 +6,13 @@ steps %Q{
   And I wait for 1 second
 }
 end
+
+When("I build the {string} variantOutput for {string} using the {string} bugsnag config") do |variant, module_config, bugsnag_config|
+steps %Q{
+  When I set environment variable "VARIANT_OUTPUT_NAME" to "#{variant}"
+  When I set environment variable "MODULE_CONFIG" to "#{module_config}"
+  When I set environment variable "BUGSNAG_CONFIG" to "#{bugsnag_config}"
+  And I run the script "features/scripts/upload_variant_mapping.sh" synchronously
+  And I wait for 1 second
+}
+end
