87122c9d4f5f661f61a40edf6e2c10544b39fcd6
fractalwrench <fractalwrench@gmail.com>
2018-03-27 13:03:51 +0100

    update origin url

--- features/scripts/build_project_module.sh
+++ features/scripts/build_project_module.sh
@@ -2,7 +2,7 @@
 
 # Alter git remote origin url in case protocol differs
 PREV_REMOTE=`git config --get remote.origin.url`
-git config --replace-all remote.origin.url "https://github.com/bugsnag/agp.git"
+git config --replace-all remote.origin.url "https://github.com/bugsnag/bugsnag-android-gradle-plugin.git"
 
 function resetGitConfig {
   git config --replace-all remote.origin.url $PREV_REMOTE
