98431022260ce970f54bd1f820d29f864733a9d1
Delisa Mason <iskanamagus@gmail.com>
2018-04-02 19:39:53 -0700

    Build plugin before test app
    Make test plugin version even more implausible

--- features/scripts/build_ndk_app.sh
+++ features/scripts/build_ndk_app.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
+BUILD_VERSION=9000.0.0-test
+
+# Build plugin
+./gradlew build -Pversion=$BUILD_VERSION
+# Install to maven local
+mvn install:install-file \
+    -Dfile=build/libs/bugsnag-android-gradle-plugin-$BUILD_VERSION.jar  \
+    -DgroupId=com.bugsnag -DartifactId=bugsnag-android-gradle-plugin \
+    -Dversion=$BUILD_VERSION \
+    -Dpackaging=jar
+
+# Build test app
 cd features/fixtures/ndkapp
-./gradlew :app:clean :app:build --stacktrace
+./gradlew :app:clean :app:build \
+    -Pplugin_version=$BUILD_VERSION \
+    --stacktrace --refresh-dependencies
