27705a324fcff94ae5f3c2c255122f751557c5d1
fractalwrench <fractalwrench@gmail.com>
2020-07-07 11:44:24 +0100

    feat: setup support for writing plugin in Kotlin

---
+++ src/main/kotlin/com.bugsnag.android.gradle/Call.kt
@@ -0,0 +1,48 @@
+package com.bugsnag.android.gradle
+
+import org.gradle.api.Project
+
+internal abstract class Call protected constructor(// 60 seconds
+    private val project: Project) {
+
+    /**
+     * Attempts to upload to the server, using automatic retries if unsuccessful
+     */
+    fun execute() {
+        var uploadSuccessful = makeApiCall()
+        val maxRetryCount = retryCount
+        var retryCount = maxRetryCount
+        while (!uploadSuccessful && retryCount > 0) {
+            project.logger.warn(String.format("Retrying Bugsnag upload (%d/%d) ...",
+                maxRetryCount - retryCount + 1, maxRetryCount))
+            uploadSuccessful = makeApiCall()
+            retryCount--
+        }
+    }
+
+    /**
+     * Makes a request to an API. If the call is not successful, clients should return false for
+     * automatic retry logic.
+     *
+     * @return true if successful, otherwise false.
+     */
+    abstract fun makeApiCall(): Boolean
+
+    /**
+     * Get the retry count defined by the user. If none is set the default is 0 (zero).
+     * Also to avoid too much retries the max value is 5 (five).
+     *
+     * @return the retry count
+     */
+    val retryCount: Int
+        get() {
+            val bugsnag = project.extensions.findByName("bugsnag") as BugsnagPluginExtension?
+            val retryCount = bugsnag!!.retryCount
+            return if (retryCount >= MAX_RETRY_COUNT) MAX_RETRY_COUNT else retryCount
+        }
+
+    companion object {
+        const val MAX_RETRY_COUNT = 5
+    }
+
+}
---
+++ src/main/kotlin/com.bugsnag.android.gradle/SourceControl.kt
@@ -0,0 +1,7 @@
+package com.bugsnag.android.gradle
+
+open class SourceControl {
+    var provider: String? = null
+    var repository: String? = null
+    var revision: String? = null
+}
