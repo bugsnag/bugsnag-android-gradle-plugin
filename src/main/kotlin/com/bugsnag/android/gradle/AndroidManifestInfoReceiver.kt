ab73691ed083728a991aa43255a94bc61738816a
Zac Sweers <pandanomic@gmail.com>
2021-03-12 11:52:24 -0500

    Add properties

--- src/main/kotlin/com/bugsnag/android/gradle/AndroidManifestInfoReceiver.kt
+++ src/main/kotlin/com/bugsnag/android/gradle/AndroidManifestInfoReceiver.kt
@@ -3,9 +3,19 @@ package com.bugsnag.android.gradle
 import org.gradle.api.Task
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity.NONE
 
 interface AndroidManifestInfoReceiver : Task {
+    @get:PathSensitive(NONE)
+    @get:InputFile
     val manifestInfoFile: RegularFileProperty
+
+    @get:Optional
+    @get:Input
     val versionCode: Property<Int>
 }
 
