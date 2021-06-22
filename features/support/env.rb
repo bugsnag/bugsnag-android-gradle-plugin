e6065a11a0daee66e7ab2748be8c5740c3b6f270
Alex Moinet <alex@bugsnag.com>
2021-06-22 20:14:56 +0100

    Maze-v5: Update steps and env to use v5 syntax/methods

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,16 +1,18 @@
 # Configure app environment
+# Set this explicitly
+$api_key = 'TEST_API_KEY'
 
 # Set which test fixture should be used
-ENV["APP_FIXTURE_DIR"] ||= "features/fixtures/app"
-ENV["NDK_FIXTURE_DIR"] ||= "features/fixtures/ndkapp"
-ENV["RN_FIXTURE_DIR"] ||= "features/fixtures/rn063/android"
-ENV["RN_MONOREPO_FIXTURE_DIR"] ||= "features/fixtures/rn-monorepo/abc/android"
-ENV["UNITY_2018_FIXTURE_DIR"] ||= "features/fixtures/unity_2018/example"
-ENV["UNITY_2019_FIXTURE_DIR"] ||= "features/fixtures/unity_2019"
+ENV['APP_FIXTURE_DIR'] ||= 'features/fixtures/app'
+ENV['NDK_FIXTURE_DIR'] ||= 'features/fixtures/ndkapp'
+ENV['RN_FIXTURE_DIR'] ||= 'features/fixtures/rn063/android'
+ENV['RN_MONOREPO_FIXTURE_DIR'] ||= 'features/fixtures/rn-monorepo/abc/android'
+ENV['UNITY_2018_FIXTURE_DIR'] ||= 'features/fixtures/unity_2018/example'
+ENV['UNITY_2019_FIXTURE_DIR'] ||= 'features/fixtures/unity_2019'
 
 # set defaults for versions
-ENV["AGP_VERSION"] ||= "4.1.0" # default to latest
-ENV["GRADLE_WRAPPER_VERSION"] ||= "6.5.1"
+ENV['AGP_VERSION'] ||= '4.1.0' # default to latest
+ENV['GRADLE_WRAPPER_VERSION'] ||= '6.5.1'
 
 `./features/scripts/clear_local_maven_repo.sh`
 `./features/scripts/setup_gradle_wrapper.sh`
@@ -33,44 +35,10 @@ Before('@skip_agp4_1_or_higher') do |scenario|
 end
 
 Before('@skip_gradle_7_or_higher') do |scenario|
-  version = ENV["GRADLE_WRAPPER_VERSION"].slice(0, 1)
+  version = ENV['GRADLE_WRAPPER_VERSION'].slice(0, 1)
   skip_this_scenario if version.to_i >= 7
 end
 
 Before('@skip_agp3_4_0') do |scenario|
   skip_this_scenario if equals_target(340)
-end
-
-Before('@skip_rn60_fixture') do |scenario|
-  skip_this_scenario if equals_rn_fixture('rn060')
-end
-
-def equals_target(target)
-  version = ENV["AGP_VERSION"].slice(0, 5)
-  version = version.gsub(".", "")
-  return version.to_i == target
-end
-
-def is_above_or_equal_to_target(target)
-  version = ENV["AGP_VERSION"].slice(0, 5)
-  version = version.gsub(".", "")
-  return version.to_i >= target
-end
-
-def equals_rn_fixture(target)
-    fixture_name = ENV["RN_FIXTURE_DIR"].split('/')[-2]
-    return fixture_name == target
-end
-
-def get_requests_with_field(name)
-  Server.stored_requests.reject do |request|
-    value = read_key_path(request[:body], name)
-    value.nil?
-  end
-end
-
-$api_key = "TEST_API_KEY"
-
-AfterConfiguration do |_config|
-  MazeRunner.config.enforce_bugsnag_integrity = false
 end
