7b60703899d0745171d7dc560e9ff07adb0c544e
Alex Moinet <alex@bugsnag.com>
2021-10-27 12:36:51 +0100

    Move tests to buildkite (#447)
    * Testing/Buildkite: Move unit and integration tests to buildkite
    
    * Testing/Buildkite: Use correct yaml argument listing
    
    * Testing/Buildkite: Fix gradle installation problems with test image
    
    * Testing/Buildkite: Move e2e tests to buildkite
    
    * Testing/Buildkite: Ensure compatability with Maze v6
    
    * Testing/Buildkite: Refactor docker image to remove upstream image as dependency
    
    * Testing/Buildkite: Add script for generating gpg keys for gradle
    
    * Testing/Buildkite: Enforce use of JDK 11
    
    * Testing/Buildkite: Enable better docker build caching
    
    * Testing/Buildkite: Ensure parity with cached android-common image
    
    * Testing/Buildkite: Refactor pipeline for gradle check parallelisation
    
    * Testing/Buildkite: Use correct gradle task for unit tests

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source "https://rubygems.org"
 
-gem "bugsnag-maze-runner", git: 'https://github.com/bugsnag/maze-runner', tag: 'v5.6.0'
+gem "bugsnag-maze-runner", git: 'https://github.com/bugsnag/maze-runner', tag: 'v6.2.0'
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,26 +1,26 @@
-# Configure app environment
-# Set this explicitly
-$api_key = 'TEST_API_KEY'
+BeforeAll do
+  # Configure app environment
+  # Set this explicitly
+  $api_key = 'TEST_API_KEY'
 
-# Set which test fixture should be used
-ENV["APP_FIXTURE_DIR"] ||= "features/fixtures/app"
-ENV["NDK_FIXTURE_DIR"] ||= "features/fixtures/ndkapp"
-ENV["RN_FIXTURE_DIR"] ||= "features/fixtures/rn063/android"
-ENV["LIB_FIXTURE_DIR"] ||= "features/fixtures/mylib"
-ENV["RN_MONOREPO_FIXTURE_DIR"] ||= "features/fixtures/rn-monorepo/abc/android"
-ENV["UNITY_2018_FIXTURE_DIR"] ||= "features/fixtures/unity_2018/example"
-ENV["UNITY_2019_FIXTURE_DIR"] ||= "features/fixtures/unity_2019"
+  # Set which test fixture should be used
+  ENV["APP_FIXTURE_DIR"] ||= "features/fixtures/app"
+  ENV["NDK_FIXTURE_DIR"] ||= "features/fixtures/ndkapp"
+  ENV["RN_FIXTURE_DIR"] ||= "features/fixtures/rn063/android"
+  ENV["LIB_FIXTURE_DIR"] ||= "features/fixtures/mylib"
+  ENV["RN_MONOREPO_FIXTURE_DIR"] ||= "features/fixtures/rn-monorepo/abc/android"
+  ENV["UNITY_2018_FIXTURE_DIR"] ||= "features/fixtures/unity_2018/example"
+  ENV["UNITY_2019_FIXTURE_DIR"] ||= "features/fixtures/unity_2019"
 
-# set defaults for versions
-ENV['AGP_VERSION'] ||= '4.1.0' # default to latest
-ENV['GRADLE_WRAPPER_VERSION'] ||= '6.5.1'
+  # set defaults for versions
+  ENV['AGP_VERSION'] ||= '4.1.0' # default to latest
+  ENV['GRADLE_WRAPPER_VERSION'] ||= '6.5.1'
 
-AfterConfiguration do |_config|
   Maze.config.enforce_bugsnag_integrity = false
 
   Maze::Runner.run_command('./features/scripts/clear_local_maven_repo.sh')
-  Maze::Runner.run_command('./features/scripts/setup_gradle_wrapper.sh')
   Maze::Runner.run_command('./features/scripts/install_gradle_plugin.sh')
+  Maze::Runner.run_command('./features/scripts/setup_gradle_wrapper.sh')
 end
 
 Before('@requires_agp4_0_or_higher') do |scenario|
