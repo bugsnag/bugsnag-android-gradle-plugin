f922435b15f95a7e0375e18729532f40cd45e507
Jason <jason.morris@bugsnag.com>
2023-02-16 11:41:51 +0000

    chore: removed unused versions from GradleUtil.kt

--- src/main/kotlin/com/bugsnag/android/gradle/internal/GradleUtil.kt
+++ src/main/kotlin/com/bugsnag/android/gradle/internal/GradleUtil.kt
@@ -16,7 +16,6 @@ import okio.source
 import org.gradle.api.DomainObjectSet
 import org.gradle.api.Project
 import org.gradle.api.Task
-import org.gradle.api.invocation.Gradle
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.MapProperty
@@ -30,23 +29,12 @@ import org.gradle.util.VersionNumber
 import java.io.File
 
 internal object GradleVersions {
-    val VERSION_5_3: VersionNumber = VersionNumber.parse("5.3")
-    val VERSION_6: VersionNumber = VersionNumber.parse("6.0")
     val VERSION_6_1: VersionNumber = VersionNumber.parse("6.1")
-    val VERSION_6_6: VersionNumber = VersionNumber.parse("6.6")
 }
-
-internal fun Gradle.versionNumber(): VersionNumber = VersionNumber.parse(gradleVersion)
 
 internal object AgpVersions {
     // Use baseVersion to avoid any qualifiers like `-alpha06`
     val CURRENT: VersionNumber = VersionNumber.parse(ANDROID_GRADLE_PLUGIN_VERSION).baseVersion
-    val VERSION_3_4: VersionNumber = VersionNumber.parse("3.4.0")
-    val VERSION_3_5: VersionNumber = VersionNumber.parse("3.5.0")
-    val VERSION_4_0: VersionNumber = VersionNumber.parse("4.0.0")
-    val VERSION_4_1: VersionNumber = VersionNumber.parse("4.1.0")
-    val VERSION_4_2: VersionNumber = VersionNumber.parse("4.2.0")
-    val VERSION_7_0: VersionNumber = VersionNumber.parse("7.0.0")
     val VERSION_8_0: VersionNumber = VersionNumber.parse("8.0.0")
     val VERSION_9_0: VersionNumber = VersionNumber.parse("9.0.0")
 }
